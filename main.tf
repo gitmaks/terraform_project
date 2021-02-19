55c0cfd6b1adae41e6b6b147df1f30864d4bdce7
gitmaks <74621586+gitmaks@users.noreply.github.com>
2021-02-19 10:05:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ data "aws_availability_zones" "available" {}
 resource "aws_key_pair" "jenkins_key" {
   key_name   = "jenkins_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDmp60+GGnRIZJ9pe1F/xo7QGH7qhm23gx8ZAhVBK9Z5ysd7yyeQjMel7ZwmVYym9JWueY2eWhfJBGdnP68c2+EnAjNmZ8fsx7N9mBRYfmKjEh+wMMajZikONGk62q4a9QgrTrZCybErmNPPLdsgHwLulJ23uMWnxpDG4XGUlqMr+E1RlAYddWcpyPRND1TsGH5cy3+91SHUtFmQssTnQrPTntmUMAuFyRyAvAx94Xh0JiZi/4S1FKXwC2WMMgOC4HTQvLrC6zPkYIm9izT6LqEmZu+PxXLU5uiD8ghWyUcQ873RY8Lh3m9aa8tNv0GpOaywvymkE4p4jWnhHbOv+K+U0YLV1lVqg8m4qpPammHKpOg8/43aRDB1xTBGlpVIjTZhi7kqCj7r0DQaPy9A4KizGA7EDINaXsM6u31q+adCEjSzUrycQJutVpKezPkebpZYoMRa+qRnjS5mBH/AiNSuCH+s59GFvglZF7MkW4Nh3nVoLdGDkq/CahY+Rr3rnU="
+  tags = {
+    Name         = "Jenkins_key"
+    ResourceName = "Key_pair"
+    Owner        = "Maxim Manovitskiy"
+  }
 }
 
 resource "aws_security_group" "jenkins_group" {
@@ -33,6 +38,11 @@ resource "aws_security_group" "jenkins_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+    tags = {
+    Name         = "Jenkins_Sec_Group"
+    ResourceName = "Security_group"
+    Owner        = "Maxim Manovitskiy"
   }
 }
 
